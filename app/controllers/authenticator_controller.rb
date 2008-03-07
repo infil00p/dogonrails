6700f6decb688ccd09fe1583c1a7e73dfa7206d9
Joe Bowser <bowserj@bowserj-nitobi.(none)>
2008-03-06 20:42:57 -0800

    Adding the Controller for the Authenticator

---
+++ app/controllers/authenticator_controller.rb
@@ -0,0 +1,32 @@
+class AuthenticatorController < ApplicationController
+
+	before_filter :requires_admin
+
+	def list
+		@auths = Authenticator.find(:all)
+	end
+
+	def new
+		@auth - Authenticator.new
+	end
+
+	def create
+		@auth = Authenticator.new(params[:auth])
+	end
+
+	def update
+		@auth = Authenticator.find(params[:id])
+		@auth.update_attributes(params[:auth])
+		@auth.save!
+	end
+
+	def update
+		@auth = Authenticator.find(params[:id])
+	end
+
+	def destroy
+		@auth = Authenticator.find(params[:id])
+		@auth.destroy!
+	end
+
+end
