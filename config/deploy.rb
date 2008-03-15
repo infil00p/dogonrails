0bc241848566ea86c409d0c6b8127443e45444e3
Joe Bowser <bowserj@bowserj-nitobi.(none)>
2008-03-14 23:24:10 -0700

    Changed the way the system is setup, we should be able to now do some cool shit!

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "dogonrails"
 set :user, "bowserj"
 set :domain,  "#{ user }@auth.dogonrails.net"
 set :deploy_to, "/var/www/dogonrails"
-set :repository, "github.com:bowserj/dogonrails.git"
+set :repository, "git://github.com/bowserj/dogonrails.git"
 set :web, "apache"
 set :ssh_flags, "-p 2223"
 
@@ -16,7 +16,7 @@ namespace :vlad do
   remote_task :symlink, :roles => :app do
 	run "rm /var/www/dogonrails/current/config/database.yml"
 	run "ln -s /var/www/dogonrails/shared/database.yml /var/www/dogonrails/current/config/database.yml"
-	run "ln -s /var/www/dogonrails/shared/facebook.yml /var/www/dogonrails/current/config/facebook.yml"
+	run "ln -s /var/www/dogonrails/shared/upgrade /var/www/dogonrails/public/upgrade"
 	run "ln -s /var/www/dogonrails/shared/logos /var/www/dogonrails/current/public/logo"
 	run "ln -s /var/www/dogonrails/shared/tiny_mce /var/www/dogonrails/current/vendor/plugins/tiny_mce"
   end
