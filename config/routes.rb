ea6306f2185c3dd997ad873500cb7c1ed2611b23
Joe Bowser <bowserj@bowserj-nitobi.(none)>
2008-04-16 17:10:18 -0700

    This really should be going to the profile every time

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "user"
+  map.connect '', :controller => "user", :action => "profile"
   #
   
   # Support current protocol's URLS
