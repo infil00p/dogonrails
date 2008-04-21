e0d6b9b626e84dfbfe7d4cd578a7e12841c4b28e
Joe Bowser <bowserj@bowserj-nitobi.(none)>
2008-04-21 15:39:33 -0700

    Shortening the list of connections

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -10,16 +10,27 @@ class Connection < ActiveRecord::Base
     
     def list_by_date(page, date, node)
       if node.nil?
-        paginate :per_page => 20, :page => page, :conditions => ['used_on > ?', date]
+        paginate :per_page => 10, :page => page, :conditions => ['used_on > ?', date]
       else
-        paginate :per_page => 20, :page => page, :conditions => ['used_on > ? AND access_node_id = ?', date, node.id]
+        paginate :per_page => 10, :page => page, :conditions => ['used_on > ? AND access_node_id = ?', date, node.id]
       end
     end 
     
     def list_by_mac(page,mac,date)
-      paginate :per_page => 20, :page => page, :conditions => ['used_on > ? AND mac = ?', date, mac]
+      paginate :per_page => 10, :page => page, :conditions => ['used_on > ? AND mac = ?', date, mac]
     end
-        
+
+    def unique_by_date(date)
+       unique_macs = []
+       pool = Connection.find(:all, :conditions => ['used_on > ?', date])
+       pool.each do |conn|
+          unless unique_macs.include? conn.mac
+              unique_macs.push(conn.mac)             
+          end
+       end
+       unique_macs
+    end
+
   end
 
   def expired?
